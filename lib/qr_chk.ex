ff53f2b84960b417a8e7b618bca236362bfb7f06
pengxy <pengxy@blizzmi.com>
2017-09-05 16:02:47 +0800

    update

--- lib/qr_chk.ex
+++ lib/qr_chk.ex
@@ -31,21 +31,48 @@ defmodule QrChk do
 
   ## Examples
 
-      iex> url = QrChk.gen_qr_url(%{appid: "appid1",uid: "1001"},"9Cf1mwejO0Vy7XK0wwhnNsLw/lRE68En","https://test.com","http://192.168.10.71:4002/openapi/api/get_login_url","1504257462")
-      iex> String.contains?(url,"https://test.com")
+      iex> result = QrChk.gen_qr_url(%{appid: "appid1",uid: "1001"},"9Cf1mwejO0Vy7XK0wwhnNsLw/lRE68En","https://test.com","http://192.168.10.71:4002/openapi/api/get_login_url","1504257462")
+      iex> case result do
+      ...>   {:ok,url}->
+      ...>     String.contains?(url,"https://test.com")
+      ...>   {:error,msg}->
+      ...>     is_binary(msg)
+      ...> end
       true
-
   """
-  @spec gen_qr_url(kvs::map,secret::binary,qr_url::binary,calback_url::binary,tz::integer|binary) :: {binary,binary} | {false,binary}
+  @spec gen_qr_url(kvs::map,secret::binary,qr_url::binary,calback_url::binary,tz::integer|binary) :: {:ok,binary} | {:error,binary}
   def gen_qr_url(kvs,secret,qr_url,req_url\\"",tz \\ DateTime.to_unix(DateTime.utc_now)) do
     url = gen_sign_url(kvs,secret,req_url,tz)
     case get_req(url) do
       {true,data}->
         {filename,_bin} = gen_qr(data)
-        file_url(filename,qr_url)
-      error ->
-        error
+        {:ok,file_url(filename,qr_url)}
+      {false,error} ->
+        {:error,error}
     end
+  end
+
+  @doc """
+  获取二维码url
+
+  ## Examples
+
+      iex> {:ok,url} = QrChk.get_qr_url(%{data: "token",tz: "1500874428"},"9Cf1mwejO0Vy7XK0wwhnNsLw/lRE68En","0a66c0d2c534b14b9c65551d0bba0214","http://192.168.10.71:4002/openapi/api/qrcode")
+      iex> String.contains?(url,"http://192.168.10.71:4002/openapi/api/qrcode")
+      true
+
+  """
+  @spec get_qr_url(kvs::map,secret::binary,sign::binary,qr_url::binary) :: {:ok,binary} | {:error,binary}
+  def get_qr_url(kvs,secret,sign,qr_url) do
+    case chk_sign(kvs,secret,sign) do
+      true ->
+        {filename,_bin} = gen_qr(kvs.data)
+        {:ok,file_url(filename,qr_url)}
+      false ->
+        {:error,"sign is invaild"}
+    end
+
+
   end
 
   defp gen_qr(data,_mode \\ nil) do
